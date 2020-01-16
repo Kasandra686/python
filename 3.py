5f904400292c97d0e0feb89c4da106f34ec18c35
Kasandra686 <verazverev@yandex.ru>
2020-01-16 09:34:44 +0500

    выполнено 3 задание

--- 3.py
+++ 3.py
@@ -1,8 +1,10 @@
 num = int(input("Введите число :"))
-num = 5
 fl = 1
-while true
-    if num / fl > 0
-
-    fl = fl * 10
-
+while True:
+    if (num / fl) > 1:
+        fl = fl * 10
+    else:
+        num2 = (num * fl) + num
+        num3 = (num2 * fl) + num
+        break
+print(f" {num} + {num2} + {num3} = {num + num2 + num3}")
